3fbb82402730e632323426cd4cbe1d352e032303
jhooge <jens.hooge@gmail.com>
2014-08-06 10:10:48 +0200

    added D3 graph

--- js/barchart.js
+++ js/barchart.js
@@ -1,5 +1,4 @@
 
-
 var n = 4, // number of layers
     m = 58, // number of samples per layer
     stack = d3.layout.stack(),
@@ -8,8 +7,8 @@ var n = 4, // number of layers
     yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
 
 var margin = {top: 40, right: 10, bottom: 20, left: 10},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 650 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .domain(d3.range(m))
@@ -32,18 +31,18 @@ var xAxis = d3.svg.axis()
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var layer = svg.selectAll(".layer")
     .data(layers)
-  .enter().append("g")
+    .enter().append("g")
     .attr("class", "layer")
     .style("fill", function(d, i) { return color(i); });
 
 var rect = layer.selectAll("rect")
     .data(function(d) { return d; })
-  .enter().append("rect")
+    .enter().append("rect")
     .attr("x", function(d) { return x(d.x); })
     .attr("y", height)
     .attr("width", x.rangeBand())
